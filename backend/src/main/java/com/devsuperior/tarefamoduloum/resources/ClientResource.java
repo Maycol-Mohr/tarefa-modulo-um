dfa3a7dab1bd89830e6e496f28741f7b1d455b9e
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-09-15 21:14:07 +0200

    H2 database

--- backend/src/main/java/com/devsuperior/tarefamoduloum/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/tarefamoduloum/resources/ClientResource.java
@@ -1,26 +1,26 @@
 package com.devsuperior.tarefamoduloum.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.tarefamoduloum.entities.Client;
+import com.devsuperior.tarefamoduloum.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Ronaldo", "83726349512", 3800.00, Instant.now(), 4));
-		list.add(new Client(2L, "Zaira", "83746528343", 4500.00, Instant.now(), 6));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-
 }
