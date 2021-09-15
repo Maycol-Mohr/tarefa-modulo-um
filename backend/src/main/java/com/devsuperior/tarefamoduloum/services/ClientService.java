8baffdb1a78f20bbac41ee9f6e7d844cb99edb36
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-09-16 01:25:45 +0200

    Database seeding PART 2

--- backend/src/main/java/com/devsuperior/tarefamoduloum/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/tarefamoduloum/services/ClientService.java
@@ -20,3 +20,4 @@ public class ClientService {
 		return repository.findAll();
 	}
 }
+
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,10 +1,11 @@
-INSERT INTO tb_client (name) VALUES ('Maria Silva');
-INSERT INTO tb_client (name) VALUES ('Andre de Melo');
-INSERT INTO tb_client (name) VALUES ('Marta dos Santos');
-INSERT INTO tb_client (name) VALUES ('Roberta Goncalves');
-INSERT INTO tb_client (name) VALUES ('Marcos Fernandes');
-INSERT INTO tb_client (name) VALUES ('Bianca de Souza');
-INSERT INTO tb_client (name) VALUES ('Fernando Carvalho');
-INSERT INTO tb_client (name) VALUES ('Romario Soares');
-INSERT INTO tb_client (name) VALUES ('Zidane Nanci');
-INSERT INTO tb_client (name) VALUES ('Maradona dos Santos');
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joao de Souza', '84592384623', 3500.0, TIMESTAMP WITH TIME ZONE '2005-11-18T20:59:19Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fernanda Motta', '36532178943', 4850.0, TIMESTAMP WITH TIME ZONE '2001-05-19T12:45:16Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pablo Medeiros', '87645689070', 2800.0, TIMESTAMP WITH TIME ZONE '2021-06-15T23:22:22Z', 6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Cristiano Silva', '26798534562', 7600.0, TIMESTAMP WITH TIME ZONE '2014-02-25T22:33:33Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Romario dos Santos', '14376568909', 3100.0, TIMESTAMP WITH TIME ZONE '1999-05-30T05:50:21Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Edson de Souza', '13254687690', 5000.0, TIMESTAMP WITH TIME ZONE '2012-07-21T21:22:23Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roberta Miranda Pires', '80967534578', 9600.0, TIMESTAMP WITH TIME ZONE '2015-06-21T23:59:11Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Juvenal Medeiros', '34589012354', 4400.0, TIMESTAMP WITH TIME ZONE '2020-08-16T20:59:19Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Diego Junior', '46734213211', 2800.0, TIMESTAMP WITH TIME ZONE '2008-03-27T18:35:27Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Alba Santos dos Anjos', '98765432109', 3100.0, TIMESTAMP WITH TIME ZONE '2017-09-24T06:35:22Z', 2);
+ 
