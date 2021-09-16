52e8f8b09d92b6cbe87f144eac707900097ab943
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-09-16 11:00:33 +0200

    DTO

--- backend/src/main/java/com/devsuperior/tarefamoduloum/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/tarefamoduloum/resources/ClientResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.tarefamoduloum.entities.Client;
+import com.devsuperior.tarefamoduloum.dto.ClientDTO;
 import com.devsuperior.tarefamoduloum.services.ClientService;
 
 @RestController
@@ -19,8 +19,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
